fa951041e29af27663fdf7224edf393ff8103e33
Trajan McGill <code@trajanmcgill.com>
2019-09-21 23:19:45 -0500

    Added all the project framework with npm dev dependencies to properly build the project with grunt. Note that this is set up to run exactly as in the now-very-old previous repository, with old versions of all those npm modules. So far this is just replicating the previous repository here on GitHub. Later will want to update to use a new version of grunt and anything else. Closes: #5

---
+++ Gruntfile.js
@@ -0,0 +1,56 @@
+module.exports = function(grunt)
+{
+	// Project configuration.
+	grunt.initConfig(
+		{
+			pkg: grunt.file.readJSON("package.json"),
+
+
+			jshint:
+			{
+				options:
+				{
+					bitwise: true, browser: true, curly: false, eqeqeq: true, forin: true, immed: true, latedef: true, laxbreak: true, laxcomma: true, newcap: true,
+					noarg: true, noempty: true, nonew: true, quotmark: "double", smarttabs: true, strict: true, trailing: true, undef: true, unused: true, validthis: true
+				},
+
+				requestAnimationFrame: { src: ["Source/requestAnimationFrame.js"] }
+			}, // end jshint task definitions
+
+
+			clean:
+			{
+				requestAnimationFrame: ["Build/**/*"]
+			}, // end clean task definitions
+
+
+			copy:
+			{
+				requestAnimationFrame: { src: ["Source/requestAnimationFrame.js"], dest: "Build/requestAnimationFrame.js" }
+			}, // end copy task defitions
+
+
+			uglify:
+			{
+				options: { sequences: false, verbose: true, warnings: true },
+
+				requestAnimationFrame: { options: { banner: "/*! requestAnimationFrame.js */\n" }, src: ["Source/requestAnimationFrame.js"], dest: "Build/requestAnimationFrame.min.js" },
+				requestAnimationFrame_DeUglify: { options: { beautify: true }, src: ["Build/requestAnimationFrame.min.js"], dest: "Build/requestAnimationFrame.min.max.js" }
+			} // end uglify task definitions
+		});
+	
+	
+	// Load the plugins
+	grunt.loadNpmTasks("grunt-contrib-clean");
+	grunt.loadNpmTasks("grunt-contrib-copy");
+	grunt.loadNpmTasks("grunt-contrib-jshint");
+	grunt.loadNpmTasks("grunt-contrib-uglify");
+	
+
+	// Define the user tasks
+	grunt.registerTask("lint_all", ["jshint:requestAnimationFrame"]);
+	grunt.registerTask("clean_all", ["clean:requestAnimationFrame"]);
+	grunt.registerTask("build_all", ["copy:requestAnimationFrame", "uglify:requestAnimationFrame"]);
+	grunt.registerTask("rebuild_all", ["clean_all", "build_all"]);
+	grunt.registerTask("default", ["lint_all", "rebuild_all"]);
+};
